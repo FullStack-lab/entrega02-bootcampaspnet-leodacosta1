42706792f4cea0b0e8ab02823627cbfcdfdfe783
leodacosta1 <leonardotmcosta@gmail.com>
2024-12-11 18:47:52 -0300

    criei página inicial com estrutura e estilização básica pra listagem de comentários e respostas, com 2 comentários e 1 resposta de exemplo.

---
+++ Controllers/CommentController/CommentController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using leodacosta02.Models.CommentModel;
+using System.Web.Mvc;
+
+namespace leodacosta02.Controllers.CommentController
+{
+    public class CommentController : Controller
+    {
+        // GET: Comment
+        public ActionResult Index()
+        {
+            List<CommentModel> comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    CommentId = 1,
+                    RepliesCount = 1,
+                    User = "John",
+                    Text = "This is a comment.",
+                    CreatedAt = new DateTime(2024, 12, 11, 15, 23, 0),
+                    Replies = new List<CommentModel>
+                    {
+                        new CommentModel
+                        {
+                            CommentId = 101,
+                            RepliesCount = 0,
+                            User = "Johnson",
+                            Text = "This is a reply.",
+                            CreatedAt = DateTime.Now
+                        }
+                    }
+                },
+                new CommentModel
+                {
+                    CommentId = 1,
+                    RepliesCount = 1,
+                    User = "Jack",
+                    Text = "This is a comment.",
+                    CreatedAt = new DateTime(2024, 12, 11, 14, 23, 0)
+                },
+
+            };
+            return View(comments);
+        }
+    }
+}
---
+++ Models/CommentModel/CommentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace leodacosta02.Models.CommentModel
+{
+    public class CommentModel
+    {
+        public int CommentId { get; set; }
+        public int RepliesCount { get; set; }
+        public string User { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<CommentModel> Replies { get; set; } = new List<CommentModel>();
+    }
+}
